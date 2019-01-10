dbdbe09bf9ff629121aaa76b337d9d362f240b3b
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-01-10 10:55:13 +0100

    INIT
    YEET

---
+++ Assets/ButtonToggle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonToggle : MonoBehaviour {
+
+	public List<Button> buttons;
+
+	public void OnToggle( bool state )
+	{
+		buttons.ForEach( ( x ) => { x.interactable = state; } );
+	}
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate: MonoBehaviour
+{
+	public void Left()
+	{
+		transform.Rotate( 15, 0, 0 );
+	}
+	public void Right()
+	{
+		transform.Rotate( -15, 0, 0 );
+	}
+}
